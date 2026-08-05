d335433c3f69ace05bd095a401d7379515f4ec26
Lucas <lleitetavares@gmail.com>
2026-08-05 02:16:30 +0000

    Updates

--- assets/pencil-BzIiG1M3.js
+++ assets/pencil-BzIiG1M3.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-BXW3bsHe.js";/**
+import{c as a}from"./index-CLEPzdhY.js";/**
  * @license lucide-react v0.436.0 - ISC
  *
  * This source code is licensed under the ISC license.
